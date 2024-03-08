11229abb499df573c0ab6ddda4be843b348a62a4
IamJannik <jannik.ohme@gmail.com>
2024-03-08 13:19:36 +0100

    🪖ArmorUpdate+

--- common/src/main/java/satisfy/dragonflame/util/api/DragonFlameDoAPI.java
+++ common/src/main/java/satisfy/dragonflame/util/api/DragonFlameDoAPI.java
@@ -3,25 +3,17 @@ package satisfy.dragonflame.util.api;
 import de.cristelknight.doapi.api.DoApiAPI;
 import de.cristelknight.doapi.api.DoApiPlugin;
 import de.cristelknight.doapi.client.render.feature.CustomArmorManager;
-import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.EntityModelSet;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import satisfy.dragonflame.registry.ArmorRegistry;
 
-import java.util.Map;
 import java.util.Set;
 
 @DoApiPlugin
 public class DragonFlameDoAPI implements DoApiAPI {
     @Override
     public void registerBlocks(Set<Block> set) {
-    }
-
-    @Override
-    public <T extends LivingEntity> void registerHat(Map<Item, EntityModel<T>> models, EntityModelSet modelLoader) {
-
     }
 
     @Override
